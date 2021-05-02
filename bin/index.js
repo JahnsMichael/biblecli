c91b94e2d2208924d2844ad64cb2362f86f228d1
jahnsmichael <michael.jf.jm@gmail.com>
2021-05-02 17:02:22 +0700

    init cli

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,16 @@
 #! /usr/bin/env node
-console.log("Hello World!");
+
+const yargs = require("yargs");
+
+const usage = "\nUsage: bib <version_name> <book_name> <chapter_number> <verse_number>";
+
+const options = yargs
+  .usage(usage)
+  .option("b", {
+    alias: "books",
+    describe: "List all supported books.",
+    type: "boolean",
+    demandOption: false
+  })
+  .help(true)
+  .argv;
